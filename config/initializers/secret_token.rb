0099fc540c0b9e040624a4c9277356b34d82f249
keswick <kehollingsworth@yahoo.com>
2011-10-13 21:31:11 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slabless::Application.config.secret_token = '606c52a7c84987c424dc32302f0be84ba9d0be7fb5866bc2d3bb78a5b5740cf6bf02f82c1ec03225c1b53b9cbc14b7b8a90d9cce88c920ad0e8d7f226a42ab99'
