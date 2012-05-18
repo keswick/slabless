447b04bad4d8a5ca18ca780c29f560f7aded8bda
keswick <kehollingsworth@yahoo.com>
2012-05-18 07:05:53 -0400

    deploy bug fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ namespace :deploy do
   after "deploy:setup", "deploy:setup_config"
 
   task :symlink_config, roles: :app do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    # run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
